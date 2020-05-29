d519e694840d152bae441615063f86239b22dcb0
FiendChain <formulating.response@gmail.com>
2020-05-29 18:36:51 +1000

    Refactored 2d canvas

--- src/app/App.js
+++ src/app/App.js
@@ -5,34 +5,31 @@ import { IndexBuffer } from '../gl/IndexBuffer';
 
 import basic_shader from '../shaders/basic';
 import { Renderer } from '../gl/Renderer';
-import { Texture2D } from '../gl/Texture2D';
-import { FrameBuffer } from '../gl/FrameBuffer';
+import { GridRender } from './GridRender';
+
+const quad = {
+    vertex_data: new Float32Array([
+        -1, -1, 
+        -1,  1,
+            1, -1,
+            1,  1
+    ]),
+    index_data: new Uint32Array([
+        0, 3, 1,
+        0, 2, 3,
+    ])
+};
 
 export class App {
     constructor(gl) {
         this.gl = gl;
 
-        // gl.enable(gl.DEPTH_TEST);
-        // gl.enable(gl.BLEND);
-
         this.renderer = new Renderer(gl);
         this.shader = new Shader(gl, basic_shader.vertex, basic_shader.frag);
         this.shader.add_uniform('uDataTexture', new Uniform(loc => gl.uniform1i(loc, 0)));
 
-        let rect_data = new Float32Array([
-            -1, -1, 
-            -1,  1,
-             1, -1,
-             1,  1
-        ]);
-
-        let index_data = new Uint32Array([
-            0, 3, 1,
-            0, 2, 3,
-        ]);
-
-        this.vbo = new VertexBufferObject(gl, rect_data, gl.STATIC_DRAW);
-        this.ibo = new IndexBuffer(gl, index_data);
+        this.vbo = new VertexBufferObject(gl, quad.vertex_data, gl.STATIC_DRAW);
+        this.ibo = new IndexBuffer(gl, quad.index_data);
         
         let layout = new VertexBufferLayout(gl);
         layout.push_attribute(0, 2, gl.FLOAT, false);
@@ -40,27 +37,10 @@ export class App {
         this.vao = new VertexArrayObject(gl);
         this.vao.add_vertex_buffer(this.vbo, layout);
 
-        this.create_data();
+        this.grid = new GridRender(gl, [100, 100]);
+        this.grid.randomise();
     }
 
-    create_data() {
-        let gl = this.gl;
-
-        this.shape = [100, 100];
-        this.data = new Uint8Array(this.shape[0]*this.shape[1]);
-        this.randomise_data();
-
-
-        this.data_texture = new Texture2D(gl, this.data, this.shape);
-        this.frame_buffer = new FrameBuffer(gl);
-        this.frame_buffer.attach_texture2D(this.data_texture);
-    }
-
-    randomise_data() {
-        for (let i = 0; i < this.data.length; i++) {
-            this.data[i] = (Math.random() > 0.5) ? 255 : 0;
-        }
-    }
 
     run() {
         requestAnimationFrame(this.loop.bind(this));
@@ -73,29 +53,21 @@ export class App {
     }
 
     on_update() {
-        this.randomise_data();
+        this.grid.randomise();
+        this.grid.refresh();
     }
 
     on_render() {
         let gl = this.gl;
-        // this.frame_buffer.bind();
-        // this.data_texture.bind();
-        // // gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-        // // gl.viewport(0, 0, 800, 800);
-        // gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
-        // // // gl.drawPixels(this.shape[0], this.shape[1], gl.UNSIGNED_BYTE, this.data);
-        // this.frame_buffer.unbind();
-
-        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, this.shape[0], this.shape[1], gl.RED, gl.UNSIGNED_BYTE, this.data);
-
 
         this.shader.bind();
         this.vao.bind();
         this.ibo.bind();
-        this.data_texture.active(0);
+        this.grid.bind();
 
         this.renderer.clear();
         this.renderer.draw(this.vao, this.ibo, this.shader);
         // gl.drawElements(gl.TRIANGLES, this.ibo.count, gl.UNSIGNED_INT, 0);
     }
 }
+
---
+++ src/app/GridRender.js
@@ -0,0 +1,39 @@
+import { Texture2D } from '../gl/Texture2D';
+import { FrameBuffer } from '../gl/FrameBuffer';
+
+export class GridRender {
+    constructor(gl, shape) {
+        this.gl = gl;
+
+        this.shape = shape;
+        this.count = shape[0]*shape[1];
+        this.data = new Uint8Array(this.shape[0]*this.shape[1]);
+
+        this.data_texture = new Texture2D(gl, this.data, this.shape);
+        this.frame_buffer = new FrameBuffer(gl);
+        this.frame_buffer.attach_texture2D(this.data_texture);
+    }
+
+    randomise() {
+        for (let i = 0; i < this.count; i++) {
+            this.data[i] = (Math.random() > 0.5) ? 255 : 0;
+        }
+    }
+
+    refresh() {
+        let gl = this.gl;
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, this.shape[0], this.shape[1], gl.RED, gl.UNSIGNED_BYTE, this.data);
+    }
+
+    bind() {
+        // this.frame_buffer.bind();
+        // this.data_texture.bind();
+        // // gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+        // // gl.viewport(0, 0, 800, 800);
+        // gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
+        // // // gl.drawPixels(this.shape[0], this.shape[1], gl.UNSIGNED_BYTE, this.data);
+        // this.frame_buffer.unbind();
+
+        this.data_texture.active(0);
+    }
+}
